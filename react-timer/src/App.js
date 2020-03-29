0e269bed9c87462588e0e095fbd12cedeca38702
Kim Hanju <khj64076407@gmail.com>
2020-03-29 17:04:21 +0900

    first-commit

---
+++ react-timer/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styles/app.css";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="timerBox">
+        <div className="timerBox__time"></div>
+        <div className="timerBox__record"></div>
+        <div className="timerBox__btn"></div>
+      </div>
+    );
+  }
+}
+
+export default App;
